baff9027a99775e1fa0ad4901754010b43ee7f8b
Dionizoniz <dbijas@icecodegames.com>
2024-11-05 11:12:53 +0100

    Add warior namespace. Restore warior setup.

--- MassBattle/Assets/Scripts/Logic/Units/Warrior.cs
+++ MassBattle/Assets/Scripts/Logic/Units/Warrior.cs
@@ -1,74 +1,76 @@
 ﻿using System.Collections.Generic;
-using MassBattle.Logic.Units;
 using UnityEngine;
 
-public class Warrior : BaseUnit
+namespace MassBattle.Logic.Units
 {
-    public override void Attack(GameObject target)
+    public class Warrior : BaseUnit
     {
-        if (attackCooldown > 0)
-            return;
-
-        if (Vector3.Distance(transform.position, target.transform.position) > attackRange)
-            return;
-
-        BaseUnit targetUnit = target.GetComponentInChildren<BaseUnit>();
-
-        if (targetUnit == null)
-            return;
-
-        attackCooldown = maxAttackCooldown;
-
-        var animator = GetComponentInChildren<Animator>();
-        animator.SetTrigger("Attack");
-
-        targetUnit.Hit(gameObject);
-    }
-
-    public void OnDeathAnimFinished()
-    {
-        Destroy(gameObject);
-    }
-
-    protected override void UpdateDefensive(List<GameObject> allies, List<GameObject> enemies)
-    {
-        Vector3 enemyCenter = Utils.GetCenter(enemies);
-
-        if (Mathf.Abs(enemyCenter.x - transform.position.x) > 20)
+        public override void Attack(GameObject target)
         {
-            if (enemyCenter.x < transform.position.x)
-                Move(Vector3.left);
+            if (attackCooldown > 0)
+                return;
 
-            if (enemyCenter.x > transform.position.x)
-                Move(Vector3.right);
+            if (Vector3.Distance(transform.position, target.transform.position) > attackRange)
+                return;
+
+            BaseUnit targetUnit = target.GetComponentInChildren<BaseUnit>();
+
+            if (targetUnit == null)
+                return;
+
+            attackCooldown = maxAttackCooldown;
+
+            var animator = GetComponentInChildren<Animator>();
+            animator.SetTrigger("Attack");
+
+            targetUnit.Hit(gameObject);
         }
 
-        Utils.GetNearestObject(gameObject, enemies, out GameObject nearestObject);
-
-        if (nearestObject == null)
-            return;
-
-        if (attackCooldown <= 0)
-            Move((nearestObject.transform.position - transform.position).normalized);
-        else
+        public void OnDeathAnimFinished()
         {
-            Move((nearestObject.transform.position - transform.position).normalized * -1);
+            Destroy(gameObject);
         }
 
-        Attack(nearestObject);
-    }
+        protected override void UpdateDefensive(List<GameObject> allies, List<GameObject> enemies)
+        {
+            Vector3 enemyCenter = Utils.GetCenter(enemies);
 
-    protected override void UpdateBasic(List<GameObject> allies, List<GameObject> enemies)
-    {
-        Utils.GetNearestObject(gameObject, enemies, out GameObject nearestEnemy);
+            if (Mathf.Abs(enemyCenter.x - transform.position.x) > 20)
+            {
+                if (enemyCenter.x < transform.position.x)
+                    Move(Vector3.left);
 
-        if (nearestEnemy == null)
-            return;
+                if (enemyCenter.x > transform.position.x)
+                    Move(Vector3.right);
+            }
 
-        Vector3 toNearest = (nearestEnemy.transform.position - transform.position).normalized;
-        toNearest.Scale(new Vector3(1, 0, 1));
-        Move(toNearest.normalized);
+            Utils.GetNearestObject(gameObject, enemies, out GameObject nearestObject);
 
-        Attack(nearestEnemy);
+            if (nearestObject == null)
+                return;
+
+            if (attackCooldown <= 0)
+                Move((nearestObject.transform.position - transform.position).normalized);
+            else
+            {
+                Move((nearestObject.transform.position - transform.position).normalized * -1);
+            }
+
+            Attack(nearestObject);
+        }
+
+        protected override void UpdateBasic(List<GameObject> allies, List<GameObject> enemies)
+        {
+            Utils.GetNearestObject(gameObject, enemies, out GameObject nearestEnemy);
+
+            if (nearestEnemy == null)
+                return;
+
+            Vector3 toNearest = (nearestEnemy.transform.position - transform.position).normalized;
+            toNearest.Scale(new Vector3(1, 0, 1));
+            Move(toNearest.normalized);
+
+            Attack(nearestEnemy);
+        }
     }
 }
