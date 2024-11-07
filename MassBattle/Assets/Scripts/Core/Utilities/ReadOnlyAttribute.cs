1216fc08ad5c195947ad5f7df3d0715479114cee
Dionizoniz <greenparrotgames@gmail.com>
2024-11-07 09:45:20 +0100

    Remove summary because it do not help us

--- MassBattle/Assets/Scripts/Core/Utilities/ReadOnlyAttribute.cs
+++ MassBattle/Assets/Scripts/Core/Utilities/ReadOnlyAttribute.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace MassBattle.Core.Utilities
 {
-    /// <summary> Read Only attribute. Attribute is use only to mark ReadOnly properties. </summary>
     public class ReadOnlyAttribute : PropertyAttribute
     { }
 }
