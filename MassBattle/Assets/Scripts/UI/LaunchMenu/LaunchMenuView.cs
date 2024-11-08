4dad603044ffd6f537676dfbe5b6dbfded2ff22c
Dionizoniz <dbijas@icecodegames.com>
2024-11-08 11:13:54 +0100

    Spawn ArmyPanels based on BattleSetup. Update panel and canvas to use horizontal layout group.

--- MassBattle/Assets/Scripts/UI/LaunchMenu/LaunchMenuView.cs
+++ MassBattle/Assets/Scripts/UI/LaunchMenu/LaunchMenuView.cs
@@ -8,19 +8,31 @@ namespace MassBattle.UI.LaunchMenu
     public class LaunchMenuView : View
     {
         [SerializeField]
-        private List<ArmyPanelController> _armyPanels = new();
+        private ArmyPanelController _armyPanelToSpawn;
+        [SerializeField]
+        private RectTransform _armyPanelsRoot;
 
-        public List<ArmyPanelController> ArmyPanels => _armyPanels;
+        public List<ArmyPanelController> ArmyPanels { get; } = new();
 
         public void InitializePanels(BattleSetup battleSetup) // TODO change to SpawnPanels and adjust logic
         {
-            List<string> armyIds = battleSetup.FindAllArmySetupIds();
-
-            for (var i = 0; i < armyIds.Count || i < ArmyPanels.Count; i++)
+            foreach (var armyId in battleSetup.FindAllArmySetupIds())
             {
-                ArmySetup armySetup = battleSetup.TryFindArmySetupBy(armyIds[i]);
-                ArmyPanels[i].InitializeData(armySetup);
+                ArmySetup armySetup = battleSetup.TryFindArmySetupBy(armyId);
+
+                if (armySetup != null)
+                {
+                    SpawnArmyPanel(armySetup);
+                }
             }
+        }
+
+        private void SpawnArmyPanel(ArmySetup armySetup)
+        {
+            ArmyPanelController armyPanel = Instantiate(_armyPanelToSpawn, _armyPanelsRoot);
+            armyPanel.InitializeData(armySetup);
+
+            ArmyPanels.Add(armyPanel);
         }
     }
 }
