969f3820770be874b6e4808bab04926ad884c7f8
Dionizoniz <dbijas@icecodegames.com>
2024-11-08 11:45:39 +0100

    Add ColorDatabase for future logic and test feature.

---
+++ MassBattle/Assets/Scripts/Logic/Databases/ColorData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MassBattle.Core.Entities;
+using UnityEngine;
+
+namespace MassBattle.Logic.Databases
+{
+    [Serializable]
+    public class ColorData : IId
+    {
+        [SerializeField]
+        private string id;
+        [SerializeField]
+        private Color color = Color.white;
+
+        public string Id { get; }
+        public Color Color => color;
+    }
+}
---
+++ MassBattle/Assets/Scripts/Logic/Databases/ColorDatabase.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace MassBattle.Logic.Databases
+{
+    [CreateAssetMenu(menuName = "Databases/" + nameof(ColorDatabase), fileName = nameof(ColorDatabase), order = 0)]
+    public class ColorDatabase : BaseDatabase<ColorData>
+    { }
+}
