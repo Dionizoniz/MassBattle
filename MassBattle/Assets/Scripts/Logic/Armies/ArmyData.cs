5cde60c708c2c1394b1679538b5412a8cafa8a77
Dionizoniz <dbijas@icecodegames.com>
2024-11-08 15:03:09 +0100

    Optimize calls for getting all units.

--- MassBattle/Assets/Scripts/Logic/Armies/ArmyData.cs
+++ MassBattle/Assets/Scripts/Logic/Armies/ArmyData.cs
@@ -10,6 +10,7 @@ namespace MassBattle.Logic.Armies
 
         private readonly List<Warrior> _warriors;
         private readonly List<Archer> _archers;
+        private List<BaseUnit> _allUnits;
 
         public ArmyData enemyArmyData;
 
@@ -18,27 +19,30 @@ namespace MassBattle.Logic.Armies
             ArmySetup = armySetup;
             _warriors = warriors;
             _archers = archers;
+
+            CacheAllUnits();
+        }
+
+        private void CacheAllUnits()
+        {
+            _allUnits = new List<BaseUnit>();
+            _allUnits.AddRange(_warriors);
+            _allUnits.AddRange(_archers);
         }
 
         public void RemoveWarrior(Warrior warrior)
         {
             _warriors.Remove(warrior);
+            _allUnits.Remove(warrior);
         }
 
         public void RemoveArcher(Archer archer)
         {
             _archers.Remove(archer);
+            _allUnits.Remove(archer);
         }
 
-        public int CalculateUnitsCount() => _warriors.Count + _archers.Count;
-
-        public List<BaseUnit> FindAllUnits() // TODO Optimize
-        {
-            List<BaseUnit> units = new();
-            units.AddRange(_warriors);
-            units.AddRange(_archers);
-
-            return units;
-        }
+        public int CalculateUnitsCount() => _allUnits.Count;
+        public List<BaseUnit> FindAllUnits() => _allUnits;
     }
 }
