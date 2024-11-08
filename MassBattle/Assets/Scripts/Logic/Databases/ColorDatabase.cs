9a98aff3f95ebd8f8523a5cb3ed8a208f48844ed
Dionizoniz <dbijas@icecodegames.com>
2024-11-08 12:28:09 +0100

    Improve color database and add interface.

--- MassBattle/Assets/Scripts/Logic/Databases/ColorDatabase.cs
+++ MassBattle/Assets/Scripts/Logic/Databases/ColorDatabase.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+﻿using MassBattle.Core.Entities;
+using UnityEngine;
 
 namespace MassBattle.Logic.Databases
 {
     [CreateAssetMenu(menuName = "Databases/" + nameof(ColorDatabase), fileName = nameof(ColorDatabase), order = 0)]
-    public class ColorDatabase : BaseDatabase<ColorData>
-    { }
+    public class ColorDatabase : BaseDatabase<ColorData>, IColorDatabase
+    {
+        public int FindColorIndex(Color color)
+        {
+            return _elements.FindIndex(element => element.Color == color);
+        }
+    }
 }
---
+++ MassBattle/Assets/Scripts/Logic/Databases/IColorDatabase.cs
@@ -0,0 +1,10 @@
+﻿using MassBattle.Core.Entities;
+using UnityEngine;
+
+namespace MassBattle.Logic.Databases
+{
+    public interface IColorDatabase : IBaseDatabase<ColorData>
+    {
+        int FindColorIndex(Color color);
+    }
+}
