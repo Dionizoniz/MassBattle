312728384b34eeb54891851574d8627b7785be01
Dionizoniz <dionizoniz@gmail.com>
2024-11-11 02:52:36 +0100

    Improve BattleInstaller structure. Add logic to spawn LifeCycleProvider and add the reference in prefab.

--- MassBattle/Assets/Scripts/Logic/Installers/BattleInstaller.cs
+++ MassBattle/Assets/Scripts/Logic/Installers/BattleInstaller.cs
@@ -1,6 +1,7 @@
 ﻿using MassBattle.Core.Entities.Engine;
 using MassBattle.Logic.Armies;
 using MassBattle.Logic.BattleCreator;
+using MassBattle.Logic.Providers;
 using MassBattle.Logic.Setup;
 using UnityEngine;
 
@@ -11,15 +12,27 @@ namespace MassBattle.Logic.Installers
         [SerializeField]
         private BattleSpawner _battleSpawnerToSpawn;
         [SerializeField]
+        private LifeCycleProvider _lifeCycleProviderToSpawn;
+
+        [Space, SerializeField]
         private BattleSetup _battleSetup;
 
         private IBattleSpawner _battleSpawner;
         private IArmyProvider _armyProvider;
+        private ILifeCycleProvider _lifeCycleProvider;
 
         private void Awake()
         {
             _battleSpawner = Instantiate(_battleSpawnerToSpawn);
+            _lifeCycleProvider = Instantiate(_lifeCycleProviderToSpawn);
+
             _armyProvider = new ArmyProvider();
+        }
+
+        private void InstantiateElementsToSpawn()
+        {
+            _battleSpawner = Instantiate(_battleSpawnerToSpawn);
+            _lifeCycleProvider = Instantiate(_lifeCycleProviderToSpawn);
         }
 
         private void Start()
