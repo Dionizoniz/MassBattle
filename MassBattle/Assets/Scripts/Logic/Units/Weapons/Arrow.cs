ba552022f6427592beed039912520f7055200e81
Dionizoniz <dionizoniz@gmail.com>
2024-11-11 01:28:24 +0100

    Improve and optimize Arrow prefab. Update Arrow inspector. Add Speed dependency from Time.deltaTime. Update color sets logic. Add smaller methods. Add attack range for improve logic and increase readability.

--- MassBattle/Assets/Scripts/Logic/Units/Weapons/Arrow.cs
+++ MassBattle/Assets/Scripts/Logic/Units/Weapons/Arrow.cs
@@ -1,43 +1,60 @@
-using System;
 using MassBattle.Logic.Armies;
 using UnityEngine;
 
 namespace MassBattle.Logic.Units.Weapons
 {
-    public class Arrow : MonoBehaviour, IAttack // TODO refactor
+    public class Arrow : MonoBehaviour, IAttack
     {
-        public float speed;
+        private static readonly int COLOR = Shader.PropertyToID("_Color");
 
-        [NonSerialized]
-        public float attack;
+        [SerializeField]
+        private float _speed = 50f;
+        [SerializeField]
+        protected float _attackRange = 0.5f;
+
+        [Space, SerializeField]
+        private Renderer _renderer;
 
         private ArmyData _armyData;
         private Vector3 _target;
 
-        public float AttackValue => attack;
+        public float AttackValue { get; private set; }
         public Vector3 AttackPosition => transform.position;
 
         public void Initialize(BaseUnit sourceUnit, BaseUnit targetUnit, Color color)
         {
-            _target = targetUnit.transform.position;
-            attack = sourceUnit.AttackValue;
-            _armyData = sourceUnit.ArmyData;
-            transform.position = sourceUnit.transform.position;
+            UpdateInitialPosition(sourceUnit);
 
-            GetComponent<Renderer>().material.color = color;
+            _armyData = sourceUnit.ArmyData;
+            _target = targetUnit.transform.position;
+            AttackValue = sourceUnit.AttackValue;
+
+            UpdateColor(color);
+        }
+
+        private void UpdateInitialPosition(BaseUnit sourceUnit)
+        {
+            transform.position = sourceUnit.transform.position;
+        }
+
+        private void UpdateColor(Color color)
+        {
+            MaterialPropertyBlock propertyBlock = new();
+            propertyBlock.SetColor(COLOR, color);
+            _renderer.SetPropertyBlock(propertyBlock);
         }
 
         public void Update()
         {
             Vector3 direction = (_target - transform.position).normalized;
-            transform.position += direction * speed;
+            transform.position += direction * (_speed * Time.deltaTime);
             transform.forward = direction;
 
             foreach (var unit in _armyData.enemyArmyData.FindAllUnits())
             {
                 float dist = Vector3.Distance(unit.transform.position, transform.position);
 
-                if (dist < speed)
+                if (dist < _attackRange)
                 {
                     unit.TakeDamage(this);
                     Destroy(gameObject);
@@ -45,7 +62,7 @@ namespace MassBattle.Logic.Units.Weapons
                 }
             }
 
-            if (Vector3.Distance(transform.position, _target) < speed)
+            if (Vector3.Distance(transform.position, _target) < _attackRange)
             {
                 Destroy(gameObject);
             }
