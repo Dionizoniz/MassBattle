e31c7a067db031c8cc78de740432b2c5929289cc
Dionizoniz <dionizoniz@gmail.com>
2024-11-11 02:10:20 +0100

    Add ExtendedMonoBehaviour to optimize calling base references.

---
+++ MassBattle/Assets/Scripts/Core/Entities/Engine/ExtendedMonoBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace MassBattle.Core.Entities.Engine
+{
+    public class ExtendedMonoBehaviour : MonoBehaviour
+    {
+        private GameObject _gameObject;
+        private Transform _transform;
+
+        private void Awake()
+        { }
+
+        private void CacheReferences()
+        {
+            _gameObject = gameObject;
+            _transform = transform;
+        }
+    }
+}
