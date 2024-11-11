40a1e112f9f79dfb9f68319b7c288a693e2f81bb
Dionizoniz <dionizoniz@gmail.com>
2024-11-11 20:11:45 +0100

    Add BattleUiInstaller scripts and prefab. Add it to scene with assigned references.

---
+++ MassBattle/Assets/Scripts/UI/Installers/BattleUiInstaller.cs
@@ -0,0 +1,35 @@
+﻿using MassBattle.Core.Entities.Engine;
+using MassBattle.Logic.Installers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MassBattle.UI.Installers
+{
+    public class BattleUiInstaller : ExtendedMonoBehaviour, IBattleUiInstaller
+    {
+        [SerializeField]
+        private BattleInstaller _spawnedBattleInstaller;
+
+        //[SerializeField]
+        //private LaunchMenuController _launchMenuControllerToSpawn;
+        [SerializeField]
+        private EventSystem _eventSystemToSpawn;
+
+        private void Awake()
+        {
+            //     SpawnLaunchMenuController();
+            SpawnEventSystem();
+        }
+
+        // private void SpawnLaunchMenuController()
+        // {
+        //     ILaunchMenuController launchMenu = Instantiate(_launchMenuControllerToSpawn);
+        //     launchMenu.InjectData(_battleSetup, _colorDatabase);
+        // }
+
+        private void SpawnEventSystem()
+        {
+            Instantiate(_eventSystemToSpawn);
+        }
+    }
+}
