7939cc30b73705e6545d026af37a82f087fdb206
Dionizoniz <dbijas@icecodegames.com>
2024-11-12 15:25:21 +0100

    Add new colors to the database.

--- MassBattle/Assets/Scripts/Logic/Databases/ColorDatabase.cs
+++ MassBattle/Assets/Scripts/Logic/Databases/ColorDatabase.cs
@@ -6,6 +6,14 @@ namespace MassBattle.Logic.Databases
     [CreateAssetMenu(menuName = "Databases/" + nameof(ColorDatabase), fileName = nameof(ColorDatabase), order = 0)]
     public class ColorDatabase : BaseDatabase<ColorData>, IColorDatabase
     {
+        [Space, SerializeField]
+        private Color _damageColor = Color.red;
+        [SerializeField]
+        private Color _deathColor = Color.black;
+
+        public Color DamageColor => _damageColor;
+        public Color DeathColor => _deathColor;
+
         public int FindColorIndex(Color color)
         {
             return _elements.FindIndex(element => element.Color == color);
