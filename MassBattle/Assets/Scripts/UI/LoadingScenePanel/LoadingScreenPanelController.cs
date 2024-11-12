a777e428e05dc0593caa7943c04cc99c2a612cf7
Dionizoniz <dbijas@icecodegames.com>
2024-11-12 11:39:00 +0100

    Add MVC for loading screen and add components to prefab.

---
+++ MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelController.cs
@@ -0,0 +1,8 @@
+﻿using MassBattle.Core.Entities.MVC;
+
+namespace MassBattle.UI.LoadingScenePanel
+{
+    public class LoadingScreenPanelController : Controller<LoadingScreenPanelModel, LoadingScreenPanelView>,
+                                                ILoadingScreenPanelController
+    { }
+}
---
+++ MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MassBattle.Core.Entities.MVC;
+using MassBattle.Core.SceneLoaders;
+using UnityEngine;
+
+namespace MassBattle.UI.LoadingScenePanel
+{
+    public class LoadingScreenPanelModel : Model<LoadingScreenPanelView>
+    {
+        [SerializeField]
+        private SceneLoader _sceneLoader;
+
+        public void StartLoadingTargetScene()
+        {
+            //_sceneLoader.TargetSceneNameToLoad
+        }
+    }
+}
