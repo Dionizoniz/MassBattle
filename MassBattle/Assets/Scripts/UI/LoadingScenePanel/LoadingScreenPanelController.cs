78300f0ca289214450f1c14af138edf07671dc26
Dionizoniz <dbijas@icecodegames.com>
2024-11-12 11:45:10 +0100

    Add logic to load scene LoadSceneAsync with min loading time. Add reference to SceneLoader in prefab.

--- MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelController.cs
+++ MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelController.cs
@@ -4,5 +4,11 @@ namespace MassBattle.UI.LoadingScenePanel
 {
     public class LoadingScreenPanelController : Controller<LoadingScreenPanelModel, LoadingScreenPanelView>,
                                                 ILoadingScreenPanelController
-    { }
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+            _model.StartLoadingTargetScene();
+        }
+    }
 }
--- MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelModel.cs
+++ MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelModel.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using MassBattle.Core.Entities.MVC;
+﻿using MassBattle.Core.Entities.MVC;
 using MassBattle.Core.SceneLoaders;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MassBattle.UI.LoadingScenePanel
 {
@@ -9,10 +9,35 @@ namespace MassBattle.UI.LoadingScenePanel
     {
         [SerializeField]
         private SceneLoader _sceneLoader;
+        [SerializeField]
+        private float _minLoadingTime = 1.5f;
+
+        private AsyncOperation _loadSceneOperation;
+        private float _loadingTime;
 
         public void StartLoadingTargetScene()
         {
-            //_sceneLoader.TargetSceneNameToLoad
+            _loadSceneOperation = SceneManager.LoadSceneAsync(_sceneLoader.TargetSceneNameToLoad);
+            _loadSceneOperation.allowSceneActivation = false;
+        }
+
+        private void Update()
+        {
+            UpdateLoadingTime();
+            TryAllowSceneActivation();
+        }
+
+        private void UpdateLoadingTime()
+        {
+            _loadingTime += Time.deltaTime;
+        }
+
+        private void TryAllowSceneActivation()
+        {
+            if (_loadingTime > _minLoadingTime && _loadSceneOperation.progress >= 0.9f)
+            {
+                _loadSceneOperation.allowSceneActivation = true;
+            }
         }
     }
 }
