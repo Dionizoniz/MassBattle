3bedbcbd84f4e391a9a501e43e3caadab530794d
Dionizoniz <dbijas@icecodegames.com>
2024-11-13 12:53:05 +0100

    Add base class for tests with find asset logic. Add first working test to check BattleSetup assets.

--- MassBattle/Assets/Scripts/Tests/Editor/ProjectSetupTests.cs
+++ MassBattle/Assets/Scripts/Tests/Editor/ProjectSetupTests.cs
@@ -1,12 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using MassBattle.Core.Entities.Tests;
+using MassBattle.Logic.BattleCreator;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace Tests.Editor
 {
-    public class ProjectSetupTests
+    public class ProjectSetupTests : BaseTests
     {
         [Test]
-        public void _00_TestSetup_Installers()
+        public void _00_TestSetup_BattleSetup()
         {
+            List<BattleSetup> battleSetups = FindAssets<BattleSetup>();
+            Assert.True(IsCorrectAssetsSetup(battleSetups));
+        }
+
+        private bool IsCorrectAssetsSetup<T>(List<T> assets) where T : ICheckSetup
+        {
+            bool isCorrectSetup = true;
+            bool isAnySetup = assets.Any();
+
+            foreach (T asset in assets)
+            {
+                isCorrectSetup &= asset.IsSetupCorrect();
+            }
+
+            return isCorrectSetup && isAnySetup;
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
