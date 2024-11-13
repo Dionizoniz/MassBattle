d72fa4f894edaa3bf79ff9d461a45b58c8deec92
Dionizoniz <dbijas@icecodegames.com>
2024-11-13 11:54:10 +0100

    Improve test assembly. Add editor class for setup tests.

---
+++ MassBattle/Assets/Scripts/Tests/Editor/ProjectSetupTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace Tests.Editor
+{
+    public class ProjectSetupTests
+    {
+        [Test]
+        public void _00_TestSetup_Installers()
+        {
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        // [UnityTest]
+        // public IEnumerator NewTestScriptWithEnumeratorPasses()
+        // {
+        //     // Use the Assert class to test conditions.
+        //     // Use yield to skip a frame.
+        //     yield return null;
+        // }
+    }
+}
