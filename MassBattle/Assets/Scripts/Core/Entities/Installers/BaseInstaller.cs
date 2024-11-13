ba838cffb6a072d13c94118f0f98135963da997f
Dionizoniz <dbijas@icecodegames.com>
2024-11-13 13:04:36 +0100

    Add base installer and implement ICheckSetup interface in children's classes.

---
+++ MassBattle/Assets/Scripts/Core/Entities/Installers/BaseInstaller.cs
@@ -0,0 +1,10 @@
+﻿using MassBattle.Core.Entities.Engine;
+using MassBattle.Core.Entities.Tests;
+
+namespace MassBattle.Core.Entities.Installers
+{
+    public abstract class BaseInstaller : ExtendedMonoBehaviour, ICheckSetup
+    {
+        public abstract bool IsSetupCorrect();
+    }
+}
--- MassBattle/Assets/Scripts/UI/Installers/BattleUiInstaller.cs
+++ MassBattle/Assets/Scripts/UI/Installers/BattleUiInstaller.cs
@@ -1,4 +1,4 @@
-﻿using MassBattle.Core.Entities.Engine;
+﻿using MassBattle.Core.Entities.Installers;
 using MassBattle.Core.SceneLoaders;
 using MassBattle.Logic.Installers;
 using MassBattle.UI.EndBattlePanel;
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 
 namespace MassBattle.UI.Installers
 {
-    public class BattleUiInstaller : ExtendedMonoBehaviour, IBattleUiInstaller
+    public class BattleUiInstaller : BaseInstaller, IBattleUiInstaller
     {
         [SerializeField]
         private BattleInstaller _spawnedBattleInstaller;
@@ -45,6 +45,18 @@ namespace MassBattle.UI.Installers
         private void InjectData()
         {
             _endBattlePanel.InjectData(_spawnedBattleInstaller.ArmyProvider, _sceneLoader);
+        }
+
+        public override bool IsSetupCorrect()
+        {
+            bool isSetupCorrect = true;
+
+            isSetupCorrect &= _spawnedBattleInstaller != null;
+            isSetupCorrect &= _sceneLoader != null;
+            isSetupCorrect &= _endBattlePanelControllerToSpawn != null;
+            isSetupCorrect &= _eventSystemToSpawn != null;
+
+            return isSetupCorrect;
         }
     }
 }
