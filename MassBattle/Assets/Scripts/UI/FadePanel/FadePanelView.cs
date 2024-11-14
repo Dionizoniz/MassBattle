e83fcadfa24ec79bcc7dc95e767c6f4b473ff864
Dionizoniz <dionizoniz@gmail.com>
2024-11-14 19:22:23 +0100

    Improve face setup. Add logic to fade in / fade out instant and animate this effect. Add protection and control blocksRaycasts

--- MassBattle/Assets/Scripts/UI/FadePanel/FadePanelView.cs
+++ MassBattle/Assets/Scripts/UI/FadePanel/FadePanelView.cs
@@ -1,4 +1,5 @@
-﻿using MassBattle.Core.Entities.MVC;
+﻿using System.Collections;
+using MassBattle.Core.Entities.MVC;
 using UnityEngine;
 
 namespace MassBattle.UI.FadePanel
@@ -6,6 +7,8 @@ namespace MassBattle.UI.FadePanel
     public class FadePanelView : View
     {
         private const string ANIMATION_CURVES_TOOLTIP_MESSAGE = "X - Time in seconds, Y - alpha value.";
+        private const float MIN_ALPHA_VALUE = 0f;
+        private const float MAX_ALPHA_VALUE = 1f;
 
         [SerializeField]
         private CanvasGroup _fadeAlphaController;
@@ -15,16 +18,70 @@ namespace MassBattle.UI.FadePanel
         [SerializeField, Tooltip(ANIMATION_CURVES_TOOLTIP_MESSAGE)]
         private AnimationCurve _fadeOutSetup = AnimationCurve.Linear(0f, 1f, 0.5f, 0f);
 
+        private Coroutine _cachedFadingCoroutine;
+
         public void FadeIn(bool isInstant)
         {
-            // TODO use setup
-            _fadeAlphaController.alpha = 1;
+            if (isInstant)
+            {
+                ChangeFadeAlphaValue(MAX_ALPHA_VALUE);
+            }
+            else
+            {
+                StartFadingProcess(_fadeInSetup);
+            }
+        }
+
+        private void ChangeFadeAlphaValue(float alpha)
+        {
+            _fadeAlphaController.alpha = alpha;
+            _fadeAlphaController.blocksRaycasts = alpha >= MAX_ALPHA_VALUE;
+        }
+
+        private void StartFadingProcess(AnimationCurve fadeSetup)
+        {
+            StopFadingProcess();
+
+            _cachedFadingCoroutine = StartCoroutine(FadingProcess(fadeSetup));
+        }
+
+        private void StopFadingProcess()
+        {
+            if (_cachedFadingCoroutine != null)
+            {
+                StopCoroutine(_cachedFadingCoroutine);
+            }
+        }
+
+        private IEnumerator FadingProcess(AnimationCurve fadeSetup)
+        {
+            float time = 0f;
+
+            while (true)
+            {
+                float alphaValue = fadeSetup.Evaluate(time);
+                ChangeFadeAlphaValue(alphaValue);
+
+                time += Time.deltaTime;
+                yield return null;
+            }
         }
 
         public void FadeOut(bool isInstant)
         {
-            // TODO use setup
-            _fadeAlphaController.alpha = 0;
+            if (isInstant)
+            {
+                ChangeFadeAlphaValue(MIN_ALPHA_VALUE);
+            }
+            else
+            {
+                StartFadingProcess(_fadeOutSetup);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            StopFadingProcess();
         }
     }
 }
