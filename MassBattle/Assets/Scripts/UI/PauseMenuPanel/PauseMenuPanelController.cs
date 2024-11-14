d07aa0e7a8e183a2bb306a7730fc0936d2e513d7
Dionizoniz <dbijas@icecodegames.com>
2024-11-14 15:12:21 +0100

    Bind army provider to pause menu to hide all panel elements on end battle

--- MassBattle/Assets/Scripts/UI/PauseMenuPanel/PauseMenuPanelController.cs
+++ MassBattle/Assets/Scripts/UI/PauseMenuPanel/PauseMenuPanelController.cs
@@ -2,6 +2,7 @@ using MassBattle.Core.Entities.MVC;
 using MassBattle.Core.Providers;
 using MassBattle.Core.SceneLoaders;
 using MassBattle.Core.UserInput;
+using MassBattle.Logic.Armies;
 
 namespace MassBattle.UI.PauseMenuPanel
 {
@@ -11,19 +12,23 @@ namespace MassBattle.UI.PauseMenuPanel
         private IInputFacade _inputFacade;
         private ISceneLoader _sceneLoader;
         private IPauseGameProvider _pauseGameProvider;
+        private IArmyProvider _armyProvider;
 
-        public void InjectData(IInputFacade inputFacade, ISceneLoader sceneLoader, IPauseGameProvider pauseGameProvider)
+        public void InjectData(
+                IInputFacade inputFacade, ISceneLoader sceneLoader, IPauseGameProvider pauseGameProvider,
+                IArmyProvider armyProvider)
         {
             _inputFacade = inputFacade;
             _sceneLoader = sceneLoader;
             _pauseGameProvider = pauseGameProvider;
+            _armyProvider = armyProvider;
         }
 
         protected override void Initialize()
         {
             base.Initialize();
 
-            _view.Initialize();
+            _view.Initialize(_armyProvider);
             _model.InjectData(_inputFacade, _sceneLoader, _pauseGameProvider);
         }
 
