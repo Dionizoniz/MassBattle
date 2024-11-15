2a407f2f1a259b1f125b55630cddfe93c7ab97b6
Dionizoniz <dbijas@icecodegames.com>
2024-11-15 09:47:50 +0100

    Add logic to ArmyData, add functions to manage SavedArmiesData in database.

--- MassBattle/Assets/Scripts/Logic/Databases/ArmyDatabase/IArmyDatabase.cs
+++ MassBattle/Assets/Scripts/Logic/Databases/ArmyDatabase/IArmyDatabase.cs
@@ -1,8 +1,10 @@
-﻿using MassBattle.Core.Entities.Database;
+﻿using System.Collections.Generic;
+using MassBattle.Core.Entities.Database;
 
 namespace MassBattle.Logic.Databases.ArmyDatabase
 {
     public interface IArmyDatabase : IDatabase<InitialArmyData>
     {
+        List<ArmyData> SavedArmiesData { get; }
     }
 }
