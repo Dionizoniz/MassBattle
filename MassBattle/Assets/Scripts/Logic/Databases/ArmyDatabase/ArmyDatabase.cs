885b67d9f5f4cd1e26dc9ed490e188934226690e
Dionizoniz <dbijas@icecodegames.com>
2024-11-15 09:17:42 +0100

    Add army database and add logic for InitialArmyData. Improve tests.

--- MassBattle/Assets/Scripts/Logic/Databases/ArmyDatabase/ArmyDatabase.cs
+++ MassBattle/Assets/Scripts/Logic/Databases/ArmyDatabase/ArmyDatabase.cs
@@ -6,5 +6,16 @@ namespace MassBattle.Logic.Databases.ArmyDatabase
     [CreateAssetMenu(menuName = "Databases/" + nameof(ArmyDatabase), fileName = nameof(ArmyDatabase), order = 0)]
     public class ArmyDatabase : Database<InitialArmyData>, IArmyDatabase
     {
+        public override bool IsSetupCorrect()
+        {
+            bool isSetupCorrect = base.IsSetupCorrect();
+
+            foreach (var initialArmyData in _elements)
+            {
+                isSetupCorrect &= initialArmyData.IsSetupCorrect();
+            }
+
+            return isSetupCorrect;
+        }
     }
 }
--- MassBattle/Assets/Scripts/Logic/Databases/ArmyDatabase/InitialArmyData.cs
+++ MassBattle/Assets/Scripts/Logic/Databases/ArmyDatabase/InitialArmyData.cs
@@ -1,9 +1,40 @@
-﻿using MassBattle.Core.Entities.Database;
+﻿using System;
+using MassBattle.Core.Entities.Database;
+using MassBattle.Core.Entities.Tests;
+using MassBattle.Logic.Strategies;
+using UnityEngine;
 
 namespace MassBattle.Logic.Databases.ArmyDatabase
 {
-    public class InitialArmyData : BaseData
+    [Serializable]
+    public class InitialArmyData : BaseData, ICheckSetup
     {
+        [SerializeField]
+        private string _armyName;
+        [SerializeField]
+        private int _defaultUnitStackSize;
+        [SerializeField]
+        private StrategyType _strategyType;
+        [SerializeField]
+        private Color _armyColor;
+        [SerializeField]
+        private bool _isArmyActive;
+
+        public string ArmyName => _armyName;
+        public int DefaultUnitStackSize => _defaultUnitStackSize;
+        public StrategyType StrategyType => _strategyType;
+        public Color ArmyColor => _armyColor;
+        public bool IsArmyActive => _isArmyActive;
+
         protected override string ClassName => nameof(InitialArmyData);
+
+        public bool IsSetupCorrect()
+        {
+            bool isSetupCorrect = true;
+
+            isSetupCorrect &= string.IsNullOrEmpty(_armyName) == false;
+
+            return isSetupCorrect;
+        }
     }
 }
