27787092f21b430f2bbe582c430ce3e25c45d2be
Dionizoniz <dbijas@icecodegames.com>
2024-11-15 11:45:45 +0100

    Add UnitsCountSliderController script and prefab to adjust ui to the dynamic amount of unit types.

---
+++ MassBattle/Assets/Scripts/UI/Components/UnitsCountSliderController.cs
@@ -0,0 +1,31 @@
+﻿using MassBattle.Core.Entities.Engine;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MassBattle.UI.Components
+{
+    public class UnitsCountSliderController : ExtendedMonoBehaviour
+    {
+        [SerializeField]
+        private Slider _slider;
+        [SerializeField]
+        private TextMeshProUGUI _countLabel;
+
+        public string UnitId { get; private set; }
+        public int Count => (int)_slider.value;
+
+        public void Initialize(string unitId, int count)
+        {
+            UnitId = unitId;
+
+            _slider.SetValueWithoutNotify(count);
+            UpdateCountLabel(count);
+        }
+
+        public void UpdateCountLabel(float value)
+        {
+            _countLabel.text = value.ToString();
+        }
+    }
+}
