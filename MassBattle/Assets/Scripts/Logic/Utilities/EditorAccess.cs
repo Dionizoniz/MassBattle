c92f7a54922a1baca4b11906d63b35fb1176eb8b
Dionizoniz <dbijas@icecodegames.com>
2024-11-27 10:22:23 +0100

    Improve the EditorAccess approach, and add asset loading by path. Move EditorAccess to a better namespace and move assets to a better folder. Adjust code to use the new solution.

---
+++ MassBattle/Assets/Scripts/Logic/Utilities/EditorAccess.cs
@@ -0,0 +1,35 @@
+using MassBattle.Core.Entities.Engine;
+using MassBattle.Core.SceneLoaders;
+using UnityEditor;
+using UnityEngine;
+
+namespace MassBattle.Logic.Utilities
+{
+    [CreateAssetMenu(menuName = nameof(EditorAccess), fileName = nameof(EditorAccess), order = 0)]
+    public class EditorAccess : ExtendedScriptableObject
+    {
+        private const string PATH = "Assets/Content/Setup/EditorAccess.asset";
+
+        [SerializeField]
+        private SceneLoader _sceneLoader;
+
+        public ISceneLoader SceneLoader => _sceneLoader;
+
+        private static EditorAccess _instance;
+
+        public static EditorAccess Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+#if UNITY_EDITOR
+                    _instance = AssetDatabase.LoadAssetAtPath(PATH, typeof(EditorAccess)) as EditorAccess;
+#endif
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
