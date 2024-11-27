a98b169e03426a1e974f216f575e9555bdb6c4ab
Dionizoniz <dbijas@icecodegames.com>
2024-11-27 11:48:46 +0100

    Add base class for descriptor.

---
+++ MassBattle/Assets/Scripts/Core/Descriptors/Descriptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MassBattle.Core.Engine;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace MassBattle.Core.Descriptors
+{
+    public abstract class Descriptor : ExtendedScriptableObject, IDescriptor
+    {
+        [SerializeField, ReadOnly]
+        private string _descriptorId;
+
+        public string DescriptorId => _descriptorId;
+
+        private void OnValidate()
+        {
+            TryGenerateDescriptorId();
+        }
+
+        private void TryGenerateDescriptorId()
+        {
+            if (string.IsNullOrEmpty(_descriptorId))
+            {
+                _descriptorId = Guid.NewGuid().ToString();
+            }
+        }
+    }
+}
