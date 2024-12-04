d1ce4a3fea14ed6fea8c0327a469f923487f416c
Dionizoniz <dbijas@icecodegames.com>
2024-12-04 12:29:23 +0100

    Add a static class with constant values

---
+++ MassBattle/Assets/Scripts/Core/Providers/ConstantValues.cs
@@ -0,0 +1,19 @@
+﻿namespace MassBattle.Core.Providers
+{
+    public static class ConstantValues
+    {
+        public const string UNITS_ROOT_NAME = "UnitsRoot";
+
+        public const string ARMY_NAME_DUPLICATES_ERROR_MESSAGE =
+                "Remove duplicates in army NAMES before starting to make it easier to identify an army.";
+
+        public const string ARMY_COLOR_DUPLICATES_ERROR_MESSAGE =
+                "Remove duplicates in army COLORS before starting to make it easier to identify an army.";
+
+        public const string DATABASE_MENU_GROUP = "Databases/";
+        public const int DATABASE_MENU_ORDER = -10000;
+
+        public const string DESCRIPTOR_MENU_GROUP = "Descriptors/";
+        public const int DESCRIPTOR_MENU_ORDER = -10000;
+    }
+}
