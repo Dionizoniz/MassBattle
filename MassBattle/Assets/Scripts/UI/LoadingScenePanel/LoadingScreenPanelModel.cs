5f2cb4ca414b426fcca38a1a8451471d6bab9a98
Dionizoniz <dbijas@icecodegames.com>
2024-12-11 10:07:31 +0100

    Install UpdateProvider, update logic to use UpdateProvider and add UpdateProviderInstaller to prefab.

--- MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelModel.cs
+++ MassBattle/Assets/Scripts/UI/LoadingScenePanel/LoadingScreenPanelModel.cs
@@ -1,4 +1,6 @@
-﻿using MassBattle.Core.Patterns.MVC;
+﻿using System;
+using MassBattle.Core.Patterns.MVC;
+using MassBattle.Core.Providers;
 using MassBattle.Core.SceneLoaders;
 using UnityEngine;
 using Zenject;
@@ -13,23 +15,21 @@ namespace MassBattle.UI.LoadingScenePanel
         private float _minLoadingTime = 1.5f;
 
         private ISceneLoader _sceneLoader;
+        private IUpdateProvider _updateProvider;
 
         private AsyncOperation _loadSceneOperation;
         private float _loadingTime;
 
         [Inject]
-        private void Construct(ISceneLoader sceneLoader)
+        private void Construct(ISceneLoader sceneLoader, IUpdateProvider updateProvider)
         {
             _sceneLoader = sceneLoader;
+            _updateProvider = updateProvider;
+
+            _updateProvider.OnUpdate += ManualUpdate;
         }
 
-        public void StartLoadingTargetScene()
-        {
-            _loadSceneOperation = _sceneLoader.LoadTargetScene();
-            _loadSceneOperation.allowSceneActivation = false;
-        }
-
-        private void Update()
+        private void ManualUpdate()
         {
             UpdateLoadingTime();
             TryAllowSceneActivation();
@@ -45,6 +45,20 @@ namespace MassBattle.UI.LoadingScenePanel
             if (_loadingTime > _minLoadingTime && _loadSceneOperation.progress >= SCENE_IS_READY_TO_LOAD_PROGRESS_VALUE)
             {
                 _loadSceneOperation.allowSceneActivation = true;
+            }
+        }
+
+        public void StartLoadingTargetScene()
+        {
+            _loadSceneOperation = _sceneLoader.LoadTargetScene();
+            _loadSceneOperation.allowSceneActivation = false;
+        }
+
+        private void OnDestroy()
+        {
+            if (_updateProvider != null)
+            {
+                _updateProvider.OnUpdate -= ManualUpdate;
             }
         }
     }
